3a99b114e43fa531b64a28144a792495041d19ca
Ashvini <ashvinikumar45@gmail.com>
2018-03-27 01:02:51 +0530

    removed option randomization from option component, changed to parent component

--- src/component/option/index.js
+++ src/component/option/index.js
@@ -2,25 +2,6 @@ import React from 'react';
 
 import './option.css'
 
-// const Option = (props) => {
-//     let sentence = props.text;
-//     let sentenceArr = sentence.split(" ");
-
-//     // randomize the array words
-//     const sentenceShuffleArr = arr => (
-//         arr
-//             .map(a => [Math.random(), a])
-//             .sort((a, b) => a[0] - b[0])
-//             .map(a => a[1])
-//     )
-
-//     return (<div className="option">
-//         {sentenceShuffleArr(sentenceArr).map(function (word) {
-//             return <button onClick={() => console.log(word)}>{word}</button>;
-//         })}
-//     </div>);
-// };
-
 function OptionList(props) {
     return (
         <div className="option">
