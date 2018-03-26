11c31ce4b8617b20bfd829bdc4b4c1807de46fe8
Ashvini <ashvinikumar45@gmail.com>
2018-03-27 01:04:30 +0530

    option correctness chaecking with sentence component

--- src/App.js
+++ src/App.js
@@ -4,43 +4,69 @@ import './App.css';
 import Option from './component/option';
 import Sentence from './component/sentence';
 import Answer from './component/answer';
+import Correct from './component/correct';
+import Incorrect from './component/incorrect';
+
+// randomizing option list
+const sentenceShuffleArr = arr => (
+  arr
+    .map(a => [Math.random(), a])
+    .sort((a, b) => a[0] - b[0])
+    .map(a => a[1])
+)
 
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
       sentence: 'Winter is coming ',
       answer: '',
-      option: ["Winter", "is", "coming"]
+      option: sentenceShuffleArr(["Winter", "is", "coming"]),
+      correct: true,
     };
 
     // this.optionClick = this.optionClick.bind(this);
   }
 
-  optionClick(event){
-    this.setState({answer: this.state.answer.concat(event + " "), option: this.state.option.filter(item => item !== event) }, () => {
-      if((this.state.option).length === 0 && this.state.sentence === this.state.answer) {
-        console.log("correct");
-      } else {
-        console.log("incorrect");
-      }
-      
+  optionClick(event) {
+    this.setState({ 
+      answer: this.state.answer.concat(event + " "), 
+      option: this.state.option.filter(item => item !== event) }, () => {
+        if ((this.state.option).length === 0 && this.state.sentence === this.state.answer) {
+          this.setState({
+            correct: true,
+          })
+        } else {
+          this.setState({
+            correct: false,
+          })
+        }
     });
   }
 
   render() {
-    return (
+    return (this.state.option.length > 0) ? (
       <div className="App">
         <div className="heading">
           <h2>Pick the words in order</h2>
         </div>
-
         <Sentence text={this.state.sentence} />
         <Answer answer={this.state.answer} />
-        <Option option={this.state.option} optionClicked={this.optionClick.bind(this)}/>
+        <Option option={this.state.option} optionClicked={this.optionClick.bind(this)} />
       </div>
-    );
+    ) : (
+      <div className="App">
+        <div className="heading">
+          <h2>Pick the words in order</h2>
+        </div>
+        <Sentence text={this.state.sentence} />
+        <Answer answer={this.state.answer} />
+        <Option option={this.state.option} optionClicked={this.optionClick.bind(this)} />
+        {this.state.correct && <Correct />}
+        {!this.state.correct && <Incorrect />}
+      </div>
+    )
   }
 }
 
