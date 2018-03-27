537c90cd2d28c7880825c10625e9719e5ea3dcc7
Ashvini <ashvinikumar45@gmail.com>
2018-03-27 15:00:15 +0530

    key error fixed

--- src/component/option/index.js
+++ src/component/option/index.js
@@ -5,7 +5,7 @@ import './option.css'
 function OptionList(props) {
     return (
         <div className="option">
-            {props.option.map((w, i) => <button onClick={() => props.optionClicked(w,i)}> {w} </button>)}
+            {props.option.map((w, i) => <button key={i} onClick={() => props.optionClicked(w,i)}> {w} </button>)}
         </div>
     )
 }
